6731687c4b7cf474560dde063044b4f479f886c0
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2023-11-13 21:10:36 -0300

    se modifica ruta de vistas

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import routes from "./infrastructure/router"
+import path from 'path'
+
 const port = process.env.PORT || 3001
 const app = express()
 app.use(cors())
@@ -10,7 +12,7 @@ app.use(express.static('tmp'))
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views',path.join(__dirname, 'views'));
 
 app.use(`/`,routes)
 
